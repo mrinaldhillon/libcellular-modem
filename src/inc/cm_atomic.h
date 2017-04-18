e6c340f3d9be36af5fef956fb7172aeaa3f09ac0
mrinaldhillon <mrinaldhillon@gmail.com>
2017-04-18 05:40:52 -0400

    * cm_manager - cm_atomic_t * Improved the api to be compatible with kernel atomic_t, gcc built in *	atomic operations as well still keeping a default reference *	counting mechanism with pthread_mutex. *	These are compile time decisions based on perprocessor flags, *	that should be set based on the target platform. *	TODO: improve the pre-processor flags.

--- src/inc/cm_atomic.h
+++ src/inc/cm_atomic.h
@@ -1,14 +1,33 @@
 #ifndef _CM_ATOMIC_H_
 #define _CM_ATOMIC_H_
 
-#include "cm_types.h"
+#if defined(__KERN_ATOMIC)
 
-int cm_atomic_int_get(const volatile int *atomic);
-void cm_atomic_int_set(volatile int *atomic, int value);
+#include <asm/atomic.h>
 
-void cm_atomic_int_inc(volatile int *atomic);
-void cm_atmomic_int_dec(volatile int *atomic);
-cm_boolean cm_atomic_int_dec_is_zero(volatile int *atomic);
-cm_boolean cm_atomic_int_is_next_dec_zero(const volatile int *atomic);
+#define CM_ATOMIC_INIT  ATOMIC_INIT
+#define cm_atomic_t	atomic_t
+#define cm_atomic_read	atomic_read
+#define cm_atomic_set	atomic_set
+#define cm_atomic_inc	atomic_inc
+#define cm_atomic_dec	atomic_dec
+#define cm_atomic_dec_and_test atomic_dec_and_test
+
+#else /* !__KERN_ATOMIC */
+
+//typedef struct cm_atomic cm_atomic_t;
+typedef struct cm_atomic {
+	volatile int value;
+} cm_atomic_t;
+#define CM_ATOMIC_INIT(i)  { (i) }
+
+int cm_atomic_read(const cm_atomic_t *v);
+void cm_atomic_set(cm_atomic_t *v, int i);
+void cm_atomic_inc(cm_atomic_t *v);
+void cm_atomic_dec(cm_atomic_t *v);
+int cm_atomic_dec_and_test(cm_atomic_t *v);
+
+#endif /* __KERN_ATOMIC */
 
 #endif /* _CM_ATOMIC_H_ */
+
--- src/lib/cm_atomic.c
+++ src/lib/cm_atomic.c
@@ -1,67 +1,107 @@
-#include "cm_atomic.h"
-#include "cm_types.h"
-#include <pthread.h>
+#include <stdlib.h>
 #include <assert.h>
+
+#if defined(__KERNEL_ATOMIC)
+
+#else /* !__KERNEL_ATOMIC */
+
+#include "cm_atomic.h"
+
+#if defined(__GCC_ATOMIC) || defined(__GCC_ATOMIC_LEGACY)
+
+int cm_atomic_read(const cm_atomic_t *v)
+{
+	assert(NULL != v);
+	__sync_synchronize();
+	v->value;
+}
+
+void cm_atomic_set(cm_atomic_t *v, int i)
+{
+	assert(NULL != v);
+	v ->value= i;
+	__sync_synchronize();
+}
+
+void cm_atomic_inc(cm_atomic_t *v)
+{
+	assert(NULL != v);
+	(void) __sync_fetch_and_add(&v->value, 1);
+}
+
+void cm_atomic_dec(cm_atomic_t *v)
+{
+	assert(NULL != v);
+	(void) __sync_fetch_and_sub(&v->value, 1);
+}
+
+int cm_atomic_dec_and_test(cm_atomic_t *v)
+{
+	assert(NULL != v);
+	return 1 == __sync_fetch_and_sub(&v->value, 1) ? 1 : 0;
+}
+#else /* !__GCC_ATOMIC && !__GCC_ATOMIC_LEGACY */
+
+/*
+ * This is the default way. It is not very fast since mutex is global,
+ * all ref, unref opterations in this namespace will take this lock.
+ * TODO: lock variable can be part of the cm_atomic_t, this way each atomic
+ * variable will have associate mutex, which shpuld speed up reference counting
+ * in muti threaded environment, where references are being taken in unrelated
+ * objects. Challenge is to achieve this wihout providing init and deinit fn.
+ * for cm_atomic_t, since that won't conform with statndard atomic api.
+ */
+
+#include <pthread.h>
 
 static pthread_mutex_t cm_atomic_lock = PTHREAD_MUTEX_INITIALIZER;
 
-int cm_atomic_int_get(const volatile int *atomic)
+int cm_atomic_read(const cm_atomic_t *v)
 {
-	int value = 0;
-
-	assert(NULL != atomic);
+	assert(NULL != v);
 	pthread_mutex_lock(&cm_atomic_lock);
-	value = *atomic;
-	pthread_mutex_unlock(&cm_atomic_lock);
-
-	return value;
-}
-
-void cm_atomic_int_set(volatile int *atomic, int value)
-{
-	assert(NULL != atomic);
-	pthread_mutex_lock(&cm_atomic_lock);
-	*atomic = value;
+	return v->value;
 	pthread_mutex_unlock(&cm_atomic_lock);
 }
 
-void cm_atomic_int_inc(volatile int *atomic)
+void cm_atomic_set(cm_atomic_t *v, int i)
 {
-	assert(NULL != atomic);
+	assert(NULL != v);
 	pthread_mutex_lock(&cm_atomic_lock);
-	*atomic += 1;
+	v->value = i;
 	pthread_mutex_unlock(&cm_atomic_lock);
 }
 
-void cm_atomic_int_dec(volatile int *atomic)
+void cm_atomic_inc(cm_atomic_t *v)
 {
-	assert(NULL != atomic);
+	assert(NULL != v);
 	pthread_mutex_lock(&cm_atomic_lock);
-	*atomic -= 1;
+	v->value += 1;
 	pthread_mutex_unlock(&cm_atomic_lock);
 }
 
-cm_boolean cm_atomic_int_dec_is_zero(volatile int *atomic)
+void cm_atomic_dec(cm_atomic_t *v)
 {
-	cm_boolean is_zero;
-
-	assert(NULL != atomic);
+	assert(NULL != v);
 	pthread_mutex_lock(&cm_atomic_lock);
-	*atomic -= 1;
-	is_zero = 0 == *atomic ? 1 : 0;
+	v->value -= 1;
+	pthread_mutex_unlock(&cm_atomic_lock);
+}
+
+int cm_atomic_dec_and_test(cm_atomic_t *v)
+{
+	int is_zero;
+
+	assert(NULL != v);
+	pthread_mutex_lock(&cm_atomic_lock);
+	v->value -= 1;
+	is_zero = 0 == v->value ? 1 : 0;
 	pthread_mutex_unlock(&cm_atomic_lock);
 
 	return is_zero;
 }
 
-cm_boolean cm_atomic_int_is_next_dec_zero(const volatile int *atomic)
-{
-	cm_boolean is_zero;
+#endif /* __GCC_ATOMIC || __GCC_ATOMIC_LEGACY */
+#endif /* __KERNEL_ATOMIC */
 
-	assert(NULL != atomic);
-	pthread_mutex_lock(&cm_atomic_lock);
-	is_zero = 0 == (*atomic - 1) ? 1 : 0;
-	pthread_mutex_unlock(&cm_atomic_lock);
 
-	return is_zero;
-}
