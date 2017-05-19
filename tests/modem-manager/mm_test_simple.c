06bc046a6c4c111693bc10674edf11a768a67ef3
mrinaldhillon <mrinaldhillon@gmail.com>
2017-05-19 02:32:53 -0400

    * Tested simple modem interface * With this interface able to crete bearer and fetch run time bearer properties

--- tests/modem-manager/mm_test_simple.c
+++ tests/modem-manager/mm_test_simple.c
@@ -5,63 +5,35 @@
 MMManager *manager;
 GDBusConnection *dbus_conn;
 GList *modems;
-MMBearerProperties *properties;
+MMSimpleConnectProperties *simple_properties;
 GError *gerr;
-GList *bearers;
-GMainLoop *loop;
 
 void handle_bearer(MMBearer *bearer)
 {
-	printf("Bearer Path %s\n", mm_bearer_get_path(bearer));
 	printf("Interface %s\n", mm_bearer_get_interface(bearer));
-
-}
-
-void list_bearers(MMModem *modem)
-{
-	GList *each = NULL;
-	bearers = mm_modem_list_bearers_sync(modem, NULL, &gerr);
-	if (gerr) {
-		printf("Error in listing bearers %s", gerr->message);
-		return;
-	}
-
-	for (each = bearers; each; each = g_list_next(each)) {
-		handle_bearer(MM_BEARER(each->data));
-	}
-	g_list_free_full(bearers, (GDestroyNotify) g_object_unref);
 
 }
 
 void handle_modem(MMObject *mmobj)
 {
 	MMModem *modem = mm_object_get_modem(mmobj);
+	MMModemSimple *simple_modem = mm_object_get_modem_simple(mmobj);
 
-	g_dbus_proxy_set_default_timeout(G_DBUS_PROXY(modem), 30 * 1000);
-	printf("Modem Path: %s\n", mm_modem_get_path(modem));
+	g_dbus_proxy_set_default_timeout(G_DBUS_PROXY(simple_modem), 30 * 1000);
+	printf("Modem Path: %s\n", mm_modem_simple_get_path(simple_modem));
 
-	if (mm_modem_get_state(modem) < MM_MODEM_STATE_DISABLED) {
-		printf("Skipping modem, its not fully initalized \n");
-		goto out_unref_modem;
-	}
-	MMBearer *bearer = mm_modem_create_bearer_sync(modem, properties,
-							   NULL, &gerr);
-	if (gerr) {
-		printf("Error in creating bearer %s", gerr->message);
-		goto out_unref_modem;
-	}
 
-	const char * bearer_path = mm_bearer_get_path(bearer);
-	printf("Created bearer %s\n", bearer_path);
-	mm_bearer_connect_sync(bearer, NULL , &gerr);
+	MMBearer * bearer = mm_modem_simple_connect_sync(simple_modem,
+					simple_properties, NULL , &gerr);
 	if (gerr) {
 		printf("Error in connecting %s", gerr->message);
-		goto out_unref_bearer;
+		goto out_unref_modem;
 	}
-	printf("Interface %s\n", mm_bearer_get_interface(bearer));
-	list_bearers(modem);
+	const char * bearer_path = mm_bearer_get_path(bearer);
+	printf("Created bearer %s\n", bearer_path);
+	handle_bearer(bearer);
 
-	mm_bearer_disconnect_sync(bearer, NULL, &gerr);
+	mm_modem_simple_disconnect_sync(simple_modem, bearer_path, NULL, &gerr);
 	if (gerr) {
 		printf("Error in disconnecting %s", gerr->message);
 		goto out_unref_bearer;
@@ -86,18 +58,17 @@ void handle_modems()
 	g_list_free_full(modems, (GDestroyNotify) g_object_unref);
 }
 
-void create_bearer_properties()
+void create_simple_properties()
 {
-	properties = mm_bearer_properties_new();
-	if (NULL == properties) {
+	simple_properties = mm_simple_connect_properties_new();
+	if (NULL == simple_properties) {
 		printf("Error in creating properties");
 		return;
 	}
 
-	mm_bearer_properties_set_apn(properties, "12655.mcs");
-	mm_bearer_properties_set_ip_type(properties, MM_BEARER_IP_FAMILY_IPV4);
+	mm_simple_connect_properties_set_apn(simple_properties, "12655.mcs");
+	mm_simple_connect_properties_set_ip_type(simple_properties, MM_BEARER_IP_FAMILY_IPV4);
 }
-
 void create_manager()
 {
 	gchar * name_owner = NULL;
@@ -120,14 +91,14 @@ void create_manager()
 		printf("Owner %s\n", name_owner);
 	}
 	g_dbus_proxy_set_default_timeout (mm_manager_peek_proxy(manager), 30 * 1000);
-	create_bearer_properties();
+	create_simple_properties();
 	if(gerr) {
 		printf("Error in creating properties %s", gerr->message);
 		goto out_unref_manager;
 	}
 	handle_modems();
 
-	g_object_unref(properties);
+	g_object_unref(simple_properties);
 out_unref_manager:
 	g_object_unref(manager);
 out_unref_dbus_conn:
